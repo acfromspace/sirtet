cc0334480e8eb06bf0d9a88fc84e6d872fea56ff
AC De Leon <andrewchristopherdeleon@gmail.com>
2017-11-23 22:39:19 -0800

    User input, prefab instantiation, boundary placed
    Game functions to a slight degree, this includes, the boundaries holding the puzzle pieces in, the puzzle pieces falling down and when they reach the bottom, a new prefab is instantiated. Next update will include the row clear and bringing all objects above down 1 row.

---
+++ Sirtet Unity Game/Assets/Scripts/Game.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+
+	// Use this for initialization
+	void Start () {
+
+        SpawnNextTetromino();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateGrid(Tetromino tetromino)
+    {
+        for (int y = 0; y < gridHeight; ++y)
+        {
+            for (int x = 0; x < gridWidth; ++x)
+            {
+                if (grid[x, y] != null)
+                {
+                    if (grid[x, y].parent == tetromino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+        }
+
+        foreach (Transform mino in tetromino.transform) {
+            Vector2 pos = Round(mino.position);
+
+            if (pos.y < gridHeight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+
+    public Transform GetTransformAtGridPosition (Vector2 pos)
+    {
+        if (pos.y > gridHeight)
+        {
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+        }
+    }
+
+    public void SpawnNextTetromino ()
+    {
+        GameObject nextTetromino = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
+    }
+
+    public bool CheckIsInsideGrid (Vector2 pos)
+    {
+        // Checks piece is within  the grid
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+
+    public Vector2 Round (Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    string GetRandomTetromino ()
+    {
+        int randomTetromino = Random.Range(1, 8);
+
+        string randomTetrominoName = "Prefabs/randomTetromino";
+
+        switch (randomTetromino)
+        {
+            case 1:
+                randomTetrominoName = "Prefabs/Tetromino_J";
+                break;
+            case 2:
+                randomTetrominoName = "Prefabs/Tetromino_L";
+                break;
+            case 3:
+                randomTetrominoName = "Prefabs/Tetromino_Long";
+                break;
+            case 4:
+                randomTetrominoName = "Prefabs/Tetromino_S";
+                break;
+            case 5:
+                randomTetrominoName = "Prefabs/Tetromino_Square";
+                break;
+            case 6:
+                randomTetrominoName = "Prefabs/Tetromino_T";
+                break;
+            case 7:
+                randomTetrominoName = "Prefabs/Tetromino_Z";
+                break;
+        }
+
+        return randomTetrominoName;
+    }
+}
